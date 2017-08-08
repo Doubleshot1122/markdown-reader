52f73fa07d36bd04aeb465270d7f664bcbb08ea2
Betty Chempananical <bettymariam@gmail.com>
2017-08-08 13:45:19 -0700

    added sample test markdown file

---
+++ test/fixtures/file1.md
@@ -0,0 +1,11 @@
+# Heading
+## Heading 2
+
+paragraph
+* Apple
+* Ball
+* Cat
+
+1. Apple
+2. Ball
+3. Cat
---
+++ test/markdown.test.js
@@ -0,0 +1,8 @@
+const chai = require("chai")
+const assert = chai.assert
+
+describe('markdown tests', () => {
+  it('testing for heading1', () => {
+    
+  })
+})
